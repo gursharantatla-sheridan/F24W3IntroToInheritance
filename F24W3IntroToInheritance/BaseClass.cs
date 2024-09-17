68ff64ecbb98712eb4a81c4cbbf09cc910f92eff
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-17 14:29:35 -0400

    Add project files.

---
+++ F24W3IntroToInheritance/BaseClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F24W3IntroToInheritance
+{
+    public class BaseClass
+    {
+        private int myPrivateVar;
+        protected int myProtectedVar;
+        public int myPublicVar;
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public void M1()
+        {
+            //myPrivateVar = 1;
+            myProtectedVar = 2;
+            myPublicVar = 3;
+        }
+    }
+}
---
+++ F24W3IntroToInheritance/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace F24W3IntroToInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass objDerived = new DerivedClass();
+            //objDerived.myPrivateVar = 1;
+            //objDerived.myProtectedVar = 2;
+            objDerived.myPublicVar = 3;
+        }
+    }
+}
